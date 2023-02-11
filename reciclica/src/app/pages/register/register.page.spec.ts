561ed7f8bedaa8fec5e017902942897a1094974d
bessonniy11 <50315006+bessonniy11@users.noreply.github.com>
2023-02-11 12:25:53 +0300

    navigate and test specs

--- reciclica/src/app/pages/register/register.page.spec.ts
+++ reciclica/src/app/pages/register/register.page.spec.ts
@@ -1,19 +1,26 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick, waitForAsync} from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { RegisterPage } from './register.page';
+import {Router} from "@angular/router";
+import {AppRoutingModule} from "../../app-routing.module";
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RegisterPage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +28,11 @@ describe('RegisterPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should go to home page on register', () => {
+    spyOn(router, 'navigate');
+    component.register();
+
+    expect(router.navigate).toHaveBeenCalledWith(['home'])
+  })
 });
